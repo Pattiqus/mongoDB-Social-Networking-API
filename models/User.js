d832eae3b280b91feb44526fb91ba39612baff9c
unknown <patticus.tv@gmail.com>
2022-04-06 11:16:25 +1000

    completed user schema+

--- models/User.js
+++ models/User.js
@@ -13,8 +13,32 @@ const userSchema = new Schema(
             match: [/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, "Please enter a valid email address"],
             required: [true, "Please enter a valid email address"],
             unique: true,
-            
-
-        }
+        },
+        thoughts: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'Thought'
+            },
+        ],
+        friends: [
+            {
+                type: Schema.Types.ObjectId,
+                ref: 'User'
+            },
+        ],
+    },
+    {
+        toJSON: {
+            virtuals: true,
+        },
+        id: false,
     }
-)
+);
+
+userSchema.virtual("friendCount").get(function() {
+    return this.friends.length;
+})
+
+const User = model('User', userSchema);
+
+module.exports = User;
