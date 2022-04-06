48fc1e511ffca08fc75525db35f1c74aef4af87e
unknown <patticus.tv@gmail.com>
2022-04-07 09:04:53 +1000

    completed models

--- models/Thought.js
+++ models/Thought.js
@@ -59,5 +59,12 @@ const thoughtSchema = new Schema(
         },
         id: false,
     },
-    
-)
+);
+
+thoughtSchema.virtual("reactionCount").get(function() {
+    return this.reactions.length
+})
+
+const Thought = model('Thought', thoughtSchema);
+
+module.exports = Thought;
