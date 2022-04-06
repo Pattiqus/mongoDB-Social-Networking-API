5b3a1e65019da7a10a1335ab1608459bf8791974
unknown <patticus.tv@gmail.com>
2022-04-06 10:06:14 +1000

    added server file, renamed thoughts routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,7 @@
 
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-const thoughtRoutes = require('./thoughtRoutes.js');
+const thoughtRoutes = require('./thoughtsRoutes.js');
 
 router.use('/users', userRoutes);
 router.use('/thoughts', thoughtRoutes);
